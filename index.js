24feadf58828595fbb858ddfb1f2d9c9fd24a8bb
LightWat <dennislai120@gmail.com>
2022-12-19 20:52:59 +0800

    add href

--- index.js
+++ index.js
@@ -18,6 +18,34 @@ app.get("/select_mode", (req, res) => {
   res.sendFile(__dirname + "/src/select_mode.html");
 });
 
+app.get("/push-up", (req, res) => {
+  res.sendFile(__dirname + "/src/push-up.html");
+});
+
+app.get("/plunk", (req, res) => {
+  res.sendFile(__dirname + "/src/plunk.html");
+});
+
+app.get("/push-up/single", (req, res) => {
+  res.sendFile(__dirname + "/src/singleTime_count.html");
+});
+
+app.get("/push-up/dual", (req, res) => {
+  res.sendFile(__dirname + "/src/doubleTime_count.html");
+});
+
+app.get("/plunk/single", (req, res) => {
+  res.sendFile(__dirname + "/src/singleTime_comp.html");
+});
+
+app.get("/plunk/dual", (req, res) => {
+  res.sendFile(__dirname + "/src/doubleTime_comp.html");
+});
+
+app.get("/push-up/dual/end", (req, res) => {
+  res.sendFile(__dirname + "/src/double_end_page_count.html");
+});
+
 // socket.io
 io.on('connection', (socket) => {
     socket.join("push-up-game");
