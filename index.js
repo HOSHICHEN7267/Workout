afed53455b202730a4b58f3f80689a5363850b0c
poyingHAHA <poying.code@gmail.com>
2022-12-29 23:18:02 +0800

    完成plunk-dual

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ io.on('connection', async(socket) => {
     io.emit(`plunk-inf-challenge-start${data.userId}`, data.distance);
   })
   socket.on("plunk-inf-challenge-stop", (data) => {
-    io.emit(`plunk-inf-challenge-stop${data.userId}`);
+    io.emit(`plunk-inf-challenge-stop`, data);
   })
   socket.on("plunk-set-challenge-end", () => {
     io.emit(`plunk-set-challenge-end`);
@@ -77,9 +77,38 @@ io.on('connection', async(socket) => {
     }
   })
   // 監聽兩人的次數
-  socket.on('push-up-count-to', (data) => [
+  socket.on('push-up-count-to', (data) => {
     io.to(data.roomName).emit('push-up-count-to', data)
-  ])
+  })
+  // =====================================================
+
+  // 多人plunk============================================
+  socket.on("join-plunk", () => {
+    for(const room of rooms) {
+      if(!io.sockets.adapter.rooms.get(room.room) || io.sockets.adapter.rooms.get(room.room).size < 2){
+        socket.join(room.room);
+
+        // 第二個人進來通知第一個人，並用第一個人的時間做設定
+        if(io.sockets.adapter.rooms.get(room.room).size === 2){
+          // 當兩人都進入房間就設定時間
+          io.to(room.room).emit("set-plunk-room", {roomTime: room.time, roomName: room.room})
+        }
+        break;
+      }
+    }
+  })
+  // 監聽輛人距離與時間
+  socket.on('plunk-distance-to', (data) => {
+    io.to(data.roomName).emit('plunk-distance-to', data)
+  })
+
+  socket.on('plunk-second-to', (data) => {
+    io.to(data.roomName).emit('plunk-second-to', data)
+  })
+
+  socket.on('plunk-goalDistance-to', (data) => {
+    io.to(data.roomName).emit('plunk-goalDistance-to', data)
+  })
   // =====================================================
 
 });
